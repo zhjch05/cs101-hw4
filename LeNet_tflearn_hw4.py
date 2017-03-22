0f718e2656eaa62825a69ab2c79e5418eb888b7c
Jincheng Zhang <zhjch05@live.com>
2017-03-22 19:51:54 -0400

    two models

--- LeNet_tflearn_hw4.py
+++ LeNet_tflearn_hw4.py
@@ -28,8 +28,8 @@ network = regression(network, optimizer='adam', learning_rate=1e-4,
 
 # Training
 model = tflearn.DNN(network, tensorboard_verbose=0)
-model.fit({'input': X}, {'target': Y}, n_epoch=8,
+model.fit({'input': X}, {'target': Y}, n_epoch=20,
            validation_set=({'input': testX}, {'target': testY}),
            snapshot_step=100, show_metric=True, run_id='convnet_mnist')
 
-model.save('hw4.tflearn')
+model.save('hw4_20.tflearn')
