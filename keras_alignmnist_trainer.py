e3d08d45e304a41b1ab586eb5acfcbb8d2cdc613
Jincheng Zhang <zhjch05@live.com>
2017-03-26 21:55:12 -0400

    keras model

---
+++ keras_alignmnist_trainer.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras import backend as K
+import numpy as np
+alignmnist = np.load('alignmnist.npz')
+x = alignmnist['x']
+y = alignmnist['y']
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+
+if K.image_data_format() == 'channels_first':
+    x= x.reshape(x.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x = x.reshape(x.shape[0], img_rows, img_cols, 1)
+    x = x.reshape(x.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x = x.astype('float32')
+x /= 255
+print('x shape:', x.shape)
+print(x.shape[0], 'train samples')
+
+# convert class vectors to binary class matrices
+y = keras.utils.to_categorical(y, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(5, 5),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Conv2D(64, kernel_size=(5, 5),
+                 activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x, y,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_split=0.20)
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model_align.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
---
+++ keras_myarc_predictor.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from keras.models import model_from_json
+
+import sys
+import glob
+import skimage.transform
+import skimage.io
+import tensorflow as tf
+import ntpath
+from skimage.color import rgb2grey
+import numpy as np
+
+sess = tf.Session()
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+loaded_model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+if(len(sys.argv)>1):
+    folder_name = sys.argv[1]
+    all_pics = glob.glob("./"+folder_name+"/*")
+    images = skimage.io.imread_collection(all_pics)
+    i=0
+    output_file = open("prediction.txt", "w")
+    for image in images:
+        # image = rgb2grey(image) # cast rgb to grayscale
+        # image = 255 - image # invert color
+        image = skimage.transform.resize(image,(28,28))
+        # skimage.io.imsave("./output/"+ntpath.basename(all_pics[i])+"_output.png",arr=image) # save resized images to visualize them
+        image = image.reshape([-1, 28, 28, 1])
+        #print(ntpath.basename(all_pics[i]),"\t",tf.argmax(model.predict(image),1).eval(session=sess)[0]) # test print
+        output_file.write("%s\t%i\n" % (ntpath.basename(all_pics[i]),tf.argmax(loaded_model.predict(image),1).eval(session=sess)[0]))
+        i+=1
+    output_file.close()
+else:
+    print("Please specify a folder name .. exiting")
+    quit()
