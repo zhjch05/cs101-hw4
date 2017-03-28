f8c0ac031c1cab0d5f34b3c39882a26779c6daaa
Jincheng Zhang <zhjch05@live.com>
2017-03-27 22:02:19 -0400

    model2 saved

--- keras_myarc_predictor.py
+++ keras_myarc_predictor.py
@@ -20,12 +20,12 @@ import numpy as np
 sess = tf.Session()
 
 # load json and create model
-json_file = open('model.json', 'r')
+json_file = open('model_2.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights("model.h5")
+loaded_model.load_weights("model_2.h5")
 print("Loaded model from disk")
 loaded_model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
