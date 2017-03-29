2e5282b3428a40e5836f1fe79552c15df09172eb
Jincheng Zhang <zhjch05@live.com>
2017-03-28 21:48:24 -0400

    pad / watershed

--- keras_myarc_predictor.py
+++ keras_myarc_predictor.py
@@ -7,6 +7,7 @@ from keras.layers.normalization import BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 from keras.models import model_from_json
+import numpy as np
 
 import sys
 import glob
@@ -16,6 +17,39 @@ import tensorflow as tf
 import ntpath
 from skimage.color import rgb2grey
 import numpy as np
+import numpy.lib
+
+def add_padding(image):
+    if image.shape[0] > image.shape[1]:
+        l_edge = image.shape[0]
+        diff = image.shape[0] - image.shape[1]
+        front = int(diff/2)
+        rear = diff - front
+        new_image = np.lib.pad(image, ((0, 0), (front, rear)), 'constant', constant_values=((0, 0),))
+        return new_image
+    elif image.shape[0] < image.shape[1]:
+        l_edge = image.shape[1]
+        diff = image.shape[1] - image.shape[0]
+        front = int(diff / 2)
+        rear = diff - front
+        new_image = np.lib.pad(image, ((front, rear), (0, 0)), 'constant', constant_values=((0, 0),))
+        return new_image
+    else:
+        return image
+
+def watershed_inner(x, threshold):
+    if x > threshold:
+        return 1
+    else:
+        return 0
+
+
+def watershed(image, threshold):
+    new_image = np.empty(image.shape)
+    for i in range(len(image)):
+        for j in range(len(image[i])):
+            new_image[i][j] = watershed_inner(image[i][j], threshold)
+    return new_image
 
 sess = tf.Session()
 
@@ -30,17 +64,29 @@ print("Loaded model from disk")
 loaded_model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
+
+test = {}
+correct = 0
+#read in annotation (test)
+with open("annotation.txt","r") as annotations:
+    for line in annotations:
+        image, label = line.split()
+        test[image] = label
+total = len(test)
+
 if(len(sys.argv)>1):
     folder_name = sys.argv[1]
     all_pics = glob.glob("./"+folder_name+"/*")
     images = skimage.io.imread_collection(all_pics)
-    i=0
     output_file = open("prediction.txt", "w")
+    i=0
     for image in images:
         # image = rgb2grey(image) # cast rgb to grayscale
         # image = 255 - image # invert color
+        image = add_padding(image)
         image = skimage.transform.resize(image,(28,28))
-        # skimage.io.imsave("./output/"+ntpath.basename(all_pics[i])+"_output.png",arr=image) # save resized images to visualize them
+        # image = watershed(image, wst)
+        # skimage.io.imsave("./output/"+ntpath.basename(all_pics[i]),arr=image) # save resized images to visualize them
         image = image.reshape([-1, 28, 28, 1])
         #print(ntpath.basename(all_pics[i]),"\t",tf.argmax(model.predict(image),1).eval(session=sess)[0]) # test print
         output_file.write("%s\t%i\n" % (ntpath.basename(all_pics[i]),tf.argmax(loaded_model.predict(image),1).eval(session=sess)[0]))
